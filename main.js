a97732446690e09740c521439b5ab7c72c1403c2
anlegent <alegent@edenschool.fr>
2024-11-08 16:18:10 +0100

    circle color and mouse move
    you can now move the circle with mouse and the circle changes color based of it's position

--- main.js
+++ main.js
@@ -4,28 +4,82 @@ let posx = 0;
 let posy = 0;
 window.addEventListener("keydown", (e) => {
   if (e.key == "ArrowRight") {
-    posx += 10;
+    posx += 5;
     player.style.left = posx + "px";
+    if (0 == posx % 2) {
+      player.style.backgroundColor = "crimson";
+    } else {
+      player.style.backgroundColor = "black";
+    }
   } else if (e.key == "ArrowLeft") {
-    posx += -10;
+    posx += -5;
     player.style.left = posx + "px";
+    if (0 == posx % 2) {
+      player.style.backgroundColor = "crimson";
+    } else {
+      player.style.backgroundColor = "black";
+    }
   } else if (e.key == "ArrowUp") {
-    posy += -10;
+    posy += -5;
     player.style.top = posy + "px";
+    if (0 == posy % 2) {
+      player.style.backgroundColor = "crimson";
+    } else {
+      player.style.backgroundColor = "black";
+    }
   } else if (e.key == "ArrowDown") {
-    posy += 10;
+    posy += 5;
     player.style.top = posy + "px";
+    if (0 == posy % 2) {
+      player.style.backgroundColor = "crimson";
+    } else {
+      player.style.backgroundColor = "black";
+    }
   } else if (e.key == "d") {
-    posx += 10;
+    posx += 5;
     player.style.left = posx + "px";
+    if (0 == posx % 2) {
+      player.style.backgroundColor = "crimson";
+    } else {
+      player.style.backgroundColor = "black";
+    }
   } else if (e.key == "q") {
-    posx += -10;
+    posx += -5;
     player.style.left = posx + "px";
+    if (0 == posx % 2) {
+      player.style.backgroundColor = "crimson";
+    } else {
+      player.style.backgroundColor = "black";
+    }
   } else if (e.key == "z") {
-    posy += -10;
+    posy += -5;
     player.style.top = posy + "px";
+    if (0 == posy % 2) {
+      player.style.backgroundColor = "crimson";
+    } else {
+      player.style.backgroundColor = "black";
+    }
   } else if (e.key == "s") {
-    posy += 10;
+    posy += 5;
     player.style.top = posy + "px";
+    if (0 == posy % 2) {
+      player.style.backgroundColor = "crimson";
+    } else {
+      player.style.backgroundColor = "black";
+    }
+  }
+});
+
+window.addEventListener("mousedown", (e) => {
+  if (e) {
+    posx = e.clientX;
+    posy = e.clientY;
+    player.style.left = posx + "px";
+    player.style.top = posy + "px";
+    if (0 == posy % 2) {
+      player.style.backgroundColor = "crimson";
+    } else {
+      player.style.backgroundColor = "black";
+    }
   }
 });
